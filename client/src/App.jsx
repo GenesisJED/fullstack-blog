79b2043efea00734d53dcde75382698f436d9646
genesis <escalonagenesis97@gmail.com>
2024-11-15 18:16:01 -0400

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, About, SignIn, SignUp, Dashboard } from './pages';
 import Projects from './pages/Projects';
 import { Header } from './components/Header';
+import { FooterCom } from './components/Footer';
+
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
         <Route path='/dashboard' element={ <Dashboard /> }></Route>
         <Route path='/projects' element={ <Projects /> }></Route>
       </Routes>
+
+      <FooterCom />
     </BrowserRouter>
   )
 }
