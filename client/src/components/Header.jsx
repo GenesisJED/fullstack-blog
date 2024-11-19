2561aa5e32f7e03ddcaa6f8fc999189a3eec556d
genesis <escalonagenesis97@gmail.com>
2024-11-19 08:46:26 -0400

    add dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export const Header = () => {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
 
     return (
         <Navbar className="border-b-2">
@@ -27,8 +30,10 @@ export const Header = () => {
                     className='w-12 h-10 hidden sm:inline'
                     color='gray'
                     pill
+                    onClick={ () => dispatch(toggleTheme())}
                 >
-                    <FaMoon />
+                    { theme === 'light' ? <FaMoon /> : <FaSun /> }
+                    
                 </Button>
 
                 {currentUser ? (
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { userSlice } from './user/userSlice';
+import { themeSlice } from './theme/themeSlice';
 import { persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  theme: themeSlice.reducer,
 });
 
 const persistConfig = {
