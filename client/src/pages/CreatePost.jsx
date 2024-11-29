a639b8b9df3b77ffe358177ebccd465e92d0f88f
genesis <escalonagenesis97@gmail.com>
2024-11-29 11:38:59 -0400

    add upload post image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,45 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { createClient } from '@supabase/supabase-js';
+import { v4 as uuidv4 } from 'uuid';
+import { getEnvVariables } from '../helpers/getEnvVariables';
+
+const { VITE_APP_SUPABASE_URL, VITE_APP_ANON_KEY, VITE_APP_CDNURL } = getEnvVariables()
+
+
+const supabase = createClient(VITE_APP_SUPABASE_URL, VITE_APP_ANON_KEY);
+const CDNURL = VITE_APP_CDNURL;
 
 export const CreatePost = () => {
+    const [file, setFile] = useState([]);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+
+    const handleUploadImage = async (e) => {
+        try {
+            if (!file) {
+                setImageUploadError('Please select an image');
+                return;
+            }
+            setImageUploadError(null);
+            console.log('Upload!');
+            const { error, data } = await supabase.storage
+                .from('images')
+                .upload(uuidv4() + ".png", file)
+
+                setFormData({ ...formData, image: data.path });
+
+        } catch (error) {
+            setImageUploadError('Image upload failed');
+            setImageUploadProgress(null);
+            console.log(error);
+        }
+    }
 
     return (
         <div className='p-3 max-w-3xl mx-auto min-h-screen'>
@@ -27,14 +64,17 @@ export const CreatePost = () => {
                     <FileInput
                         type='file'
                         accept='image/*'
+                        onChange={(e) => setFile(e.target.files[0])}
                     />
                     <Button
                         type='button'
                         gradientDuoTone='purpleToBlue'
                         size='sm'
                         outline
+                        onClick={handleUploadImage}
+                        disabled={imageUploadProgress}
                     >
-                        {/* {imageUploadProgress ? (
+                        {imageUploadProgress ? (
                             <div className='w-16 h-16'>
                                 <CircularProgressbar
                                     value={imageUploadProgress}
@@ -43,17 +83,17 @@ export const CreatePost = () => {
                             </div>
                         ) : (
                             'Upload Image'
-                        )} */}
+                        )}
                     </Button>
                 </div>
-                {/* {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+                {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
                 {formData.image && (
                     <img
-                        src={formData.image}
+                        src={CDNURL + formData.image}
                         alt='upload'
                         className='w-full h-72 object-cover'
                     />
-                )} */}
+                )}
                 <ReactQuill
                     theme='snow'
                     placeholder='Write something...'
