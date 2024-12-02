6ea0a6345846fec6064aa0a42250942d96b0cd57
genesis <escalonagenesis97@gmail.com>
2024-12-02 15:29:27 -0400

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { FooterCom } from './components/Footer';
 import { PrivateRoute } from './components/PrivateRoute';
 import { OnlyAdminPrivateRoute } from './components/OnlyAdminPrivateRoute';
 import { CreatePost } from './pages/CreatePost';
+import { UpdatePost } from './pages/UpdatePost';
 
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
         </Route>
         <Route element={ <OnlyAdminPrivateRoute/> }>
           <Route path='/create-post' element={ <CreatePost /> }></Route>
+          <Route path='/update-post/:postId' element={ <UpdatePost /> }></Route>
         </Route>
         <Route path='/projects' element={ <Projects /> }></Route>
       </Routes>
