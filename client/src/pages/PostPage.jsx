964838d77e56a01caaab040a08bdf25140b20197
genesis <escalonagenesis97@gmail.com>
2024-12-06 13:37:03 -0400

    add comment section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from '../components/CallToAction';
 import { getImagesSupabase } from '../helpers/getImagesSupabase';
+import { CommentSection } from '../components/CommentSection';
 
 const { CDNURL } = getImagesSupabase();
 
@@ -74,7 +75,7 @@ export const PostPage = () => {
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      {/* <CommentSection postId={post._id} /> */}
+      <CommentSection postId={post._id} />
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
